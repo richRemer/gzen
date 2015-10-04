ace1201e859002fd552b80e7c0a8068188a6c689
Richard Remer <rich@zingle.me>
2015-10-03 21:39:15 -0700

    gzen here command

---
+++ lib/here.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -e
+
+[ -z "$GZEN" ] && echo "use gzen to run this script" 1>&2 && exit 100
+
+[ -e "$PROFILE" ] && grep ^REPO= "$PROFILE" >/dev/null \
+    && sed -i s@^REPO=.\*@REPO=`pwd`@ "$PROFILE" \
+    || echo REPO=`pwd` >> "$PROFILE"
