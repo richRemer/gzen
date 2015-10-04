159c05f3c03da90af89926abbefc0de0ca87ed91
Richard Remer <rich@zingle.me>
2015-10-03 21:48:22 -0700

    display message after setting REPO dir

--- lib/here.sh
+++ lib/here.sh
@@ -5,3 +5,5 @@
 [ -e "$PROFILE" ] && grep ^REPO= "$PROFILE" >/dev/null \
     && sed -i s@^REPO=.\*@REPO=`pwd`@ "$PROFILE" \
     || echo REPO=`pwd` >> "$PROFILE"
+
+zenity --info --title gzen --text "gzen repo set to "`pwd` 2>/dev/null &
